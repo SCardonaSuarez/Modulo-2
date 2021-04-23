e021953be8183a4e897d59b001af32a26d78159e
Santo <santicardonas777@gmail.com>
2021-04-22 22:37:06 -0500

    Se corrigio los ejercicios

--- ejercicos2.js
+++ ejercicos2.js
@@ -1,55 +1,71 @@
 //Ejercicio 1
 
-const myPromise = Promise.then(1)
+const myPromise = Promise.resolve(1)
+
 myPromise.then((value) => {
+
   console.log(value) // log: 1
 }) 
 
 //Ejercicio 2
 
-
 const myPromise = new Promise((resolve) => {
-  resolve(value)  
+  resolve(1)  
 })
 
-//Ejercicio 3
-
-
-myPromise.resolve((value) => {
+myPromise.then((value) => {
  console.log(value + 1)  // log: 2
 })
 
-//Ejercicio 4
+//Ejercicio 3
 
 const myPromise = new Promise((resolve) => {
   resolve(1)  
 })
 
 myPromise
-  .resolve((value) => {
+  .then((value) => {
     return value + 2
   })
-  .resolve((value) => {
-   console.log(value + ???) // log: 4
-  })
+  .then((value) => {
+   console.log(value + 1) // log: 4
+})
 
+//Ejercicio 4
+
+const myPromise = new Promise((resolve, reject) => {
+  reject('Error: Operation Failed')  
+})
+
+myPromise
+  .catch((err) => {
+   console.log(err) // log: 'Error: Operation Failed'
+})
 
 //Ejercicio 5
 
-  const myPromise = new Promise((err, catch) => {
-    err('Error: Operation Failed')  
-  })
-  
-  myPromise
-    .catch((err) => {
-     console.log(err) // log: 'Error: Operation Failed'
-    })
+const callback1 = (value) => console.log('Esto es un texto:', value)
+const callback2 = (value) => console.log('Esto es un numero:', value)
 
-    const myPromise = new Promise((err, catch) => {
-      err('Error: Operation Failed')  
-    })
-    
-    myPromise
-      .catch((err) => {
-       console.log(err) // log: 'Error: Operation Failed'
-      })
+const myPromise = new Promise((resolve) => {
+  setTimeout((callback1) => {
+  
+    resolve(2)
+  }, 1000) 
+})
+
+myPromise.then(callback2) // log: Esto es un numero: 2
+
+
+
+const callback1 = (value) => console.log('Esto es un texto:', value)
+const callback2 = (value) => console.log('Esto es un numero:', value)
+
+const myPromise = new Promise ((resolve) => {
+
+  setTimeout((callback1) => {
+    resolve(2)
+  }, 2000)
+})
+
+myPromise.then(callback2)
