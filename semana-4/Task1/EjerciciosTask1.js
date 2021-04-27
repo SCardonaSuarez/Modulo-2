a48083575f77c02c7d9478516d545c61f50b98ad
Santo <santicardonas777@gmail.com>
2021-04-27 15:56:41 -0500

    Se modifico el ejercicio 4 de EjerciciosTask1.ts

--- semana-4/Task1/EjerciciosTask1.js
+++ semana-4/Task1/EjerciciosTask1.js
@@ -1,20 +1,20 @@
-
-class Alumno {
-    public nombre: string;
-    public edad: number;
-    public correo: string;
-    public promedio: number;
-  
-    constructor(nombre: string, edad: number, correo: string, promedio: number) {
-      this.nombre = nombre;
-      this.edad = edad;
-      this.correo = correo;
-      this.promedio = promedio;
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var Alumno = /** @class */ (function () {
+    function Alumno(nombre, edad, correo, promedio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+        this.promedio = promedio;
     }
-}
-
-
-const alumnos: Array<Alumno> = [
+    return Alumno;
+}());
+var alumnos = [
     { nombre: "Jose Perez", edad: 22, correo: "jose@perez.com", promedio: 6 },
     { nombre: "Juan Perez", edad: 19, correo: "juan@perez.com", promedio: 7 },
     { nombre: "Maria Yepez", edad: 21, correo: "maria@yepez.com", promedio: 9 },
@@ -27,130 +27,65 @@ const alumnos: Array<Alumno> = [
     { nombre: "Carlos Marin", edad: 23, correo: "carlos@gmail.com", promedio: 8 },
     { nombre: "Karla Medina", edad: 18, correo: "Karla@gmail.com", promedio: 3 },
     { nombre: "Daniela Medina", edad: 20, correo: "danielamedinaC@gmail.com", promedio: 7 },
-]
-
-
-
-
+];
 /* *********Ejercicio1*********** */
-
 // Devuelva una arreglo que contenga todos los estudiantes cuyo promedio es mayor a 6 puntos.
-
-const alumDatos = ((student: Alumno[]) => {
-    
-    let promedio = student.filter(prom => prom.promedio > 6 )
-    return promedio
-})
-
-console.log(alumDatos(alumnos))
-
-
-
-
-
-
+var alumDatos = (function (student) {
+    var promedio = student.filter(function (prom) { return prom.promedio > 6; });
+    return promedio;
+});
+console.log(alumDatos(alumnos));
 /* *********Ejercicio2*********** */
-
 // Devuelva una lisita de los estudiantes cuyo promedio es menor a 6 puntos y la edad es mayor a 19 años.
-
-const studentAgePro = ((student: Alumno[]) => {
-
-    let ageProm = student.filter(proAge=>  proAge.edad > 19 && proAge.promedio < 6 )
-    return ageProm
-})
-
-console.log(studentAgePro(alumnos))
-
-
-
-
-
-
+var studentAgePro = (function (student) {
+    var ageProm = student.filter(function (proAge) { return proAge.edad > 19 && proAge.promedio < 6; });
+    return ageProm;
+});
+console.log(studentAgePro(alumnos));
 /* *********Ejercicio3*********** */
-
 // Devuelva un arreglo ordenado segun el promedio de forma ascendente
-
-const proAscendente = ((student: Alumno[])=>{
-
-    let listaAlum = [...student];
+var proAscendente = (function (student) {
+    var listaAlum = __spreadArrays(student);
     // let listaAlum = Array.from(student);
-
-
-    let stuAscend = listaAlum.sort((a,b) => {
-        return a.promedio - b.promedio
-    })
-    return stuAscend
-})
-
-console.log(proAscendente(alumnos))
-
-
-
-
-
+    var stuAscend = listaAlum.sort(function (a, b) {
+        return a.promedio - b.promedio;
+    });
+    return stuAscend;
+});
+console.log(proAscendente(alumnos));
 /* *********Ejercicio4*********** */
-
 // El profesor decidio otorgar 2 puntos a todos los estudiantes cuya nota es menor o igual a 4, devuelva un arreglo que cumpla con esta condición
-
-const regaloNota = ((student:Alumno[])=>{
-
-
+var regaloNota = (function (student) {
     // const regalo = student.filter(nota => nota.promedio >= 4 ? (nota.promedio + 2) : 0)
     // return regalo
-
-    const regalo = student.map(alumno => {
-
-
-/*         let sumaPro = {
-            ...alumno,
-            promedio: alumno.promedio <=4 ? alumno.promedio += 2 : alumno.promedio,
-        
-        }
-        return sumaPro  */
+    var regalo = student.map(function (alumno) {
+        /*         let sumaPro = {
+                    ...alumno,
+                    promedio: alumno.promedio <=4 ? alumno.promedio += 2 : alumno.promedio,
+                
+                }
+                return sumaPro  */
         // return {
         //     ...alumno,
         //     promedio: alumno.promedio <=4 ? alumno.promedio += 2 : alumno.promedio,
-        
         // }
-
         return {
             nombre: alumno.nombre,
-            edad : alumno.edad,
+            edad: alumno.edad,
             correo: alumno.correo,
-            promedio: alumno.promedio <=4 ? alumno.promedio += 2 : alumno.promedio,
-        }
-
-    } )
-    return regalo
-})
-
-console.log(regaloNota(alumnos))
-
-
-
-
-
+            promedio: alumno.promedio <= 4 ? alumno.promedio += 2 : alumno.promedio
+        };
+    });
+    return regalo;
+});
+console.log(regaloNota(alumnos));
 /* *********Ejercicio5*********** */
-
 // Devuelva el promedio general de todos los alumnos
-
-
-const proGeneral = ((student:Alumno[])=> {
-
+var proGeneral = (function (student) {
     // const regalo =student.forEach(nota => nota.promedio)
     // return regalo
-
-
-    const prom = student.reduce((total, value) => total + value.promedio, 0)
-
-    let total = (prom/12).toFixed(1)
-    return `El promedio general del grupo es de ${total}`;
-
-
-})
-
-console.log(proGeneral(alumnos))
-
-
-
-
+    var prom = student.reduce(function (total, value) { return total + value.promedio; }, 0);
+    var total = (prom / 12).toFixed(1);
+    return "El promedio general del grupo es de " + total;
+});
+console.log(proGeneral(alumnos));
